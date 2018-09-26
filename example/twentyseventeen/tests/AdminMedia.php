f3b6dc341e9edcec0083786828865332413c32ad
Taylor Lovett <tlovett88@gmail.com>
2018-09-26 13:29:47 -0400

    Admin media test

---
+++ example/twentyseventeen/tests/AdminMedia.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Test that admins can manage media
+ *
+ * @package wpassure
+ */
+
+/**
+ * PHPUnit test class
+ */
+class AdminMediaTest extends \WPAssure\PHPUnit\TestCase {
+
+	/**
+	 * Check that we can add a featured image
+	 */
+	public function testFeaturedImage() {
+		$I = $this->getAnonymousUser();
+
+		$I->loginAs( 'wpsnapshots' );
+
+		$I->moveTo( 'wp-admin/post-new.php' );
+
+		$I->waitUntilElementVisible( '#title' );
+
+		$I->fillField( '#title', 'Test Post' );
+
+		$I->scrollTo( 1000, 0 );
+
+		$I->takeScreenshot();
+
+		// Set featured image
+		$I->click( '#set-post-thumbnail' );
+		$I->attachFile( '.media-modal-content input[type="file"]', __DIR__ . '/img/10up-logo.jpg' );
+
+		$I->waitUntilElementEnabled( '.media-modal-content .media-button-select' );
+
+		$I->click( '.media-modal-content .media-button-select' );
+
+		$I->waitUntilElementVisible( '#remove-post-thumbnail' );
+
+		$I->scrollTo( 0, 0 );
+
+		$I->click( '#publish' );
+
+		$I->waitUntilElementVisible( '#wpadminbar' );
+
+		// See featured image
+		$I->seeElement( '#postimagediv img' );
+	}
+
+}
