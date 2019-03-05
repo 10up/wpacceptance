0a995e72b23860fb3e1277259fa5536404d905f7
Taylor Lovett <tlovett88@gmail.com>
2019-03-05 12:26:37 -0500

    Setup env and run npm install

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -9,7 +9,7 @@ namespace WPAcceptance;
 
 use \Symfony\Component\Console\Application;
 
-$app = new Application( 'WPAcceptance', '0.12' );
+$app = new Application( 'WPAcceptance', '0.12.1' );
 
 define( 'WPACCEPTANCE_DIR', dirname( __DIR__ ) );
 
