739ccd2473180ac559dccd795e0b90a82d07e444
Taylor Lovett <tlovett88@gmail.com>
2019-03-18 10:56:05 -0400

    Twentynineteen example

---
+++ example/twentynineteen/tests/HomePage.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Test if homepage is showing properly
+ *
+ * @package wpacceptance
+ */
+
+/**
+ * PHPUnit test class
+ */
+class HomePageTest extends \WPAcceptance\PHPUnit\TestCase {
+
+	/**
+	 * @testdox I fill out search form, press enter, and the search page shows with results.
+	 */
+	public function testSearchForm() {
+		$I = $this->openBrowserPage();
+
+		$I->moveTo( '/' );
+
+		$I->fillField( '.search-form input[type=search]', 'test search' );
+
+		$I->pressEnterKey( '.search-form input[type=search]' );
+
+		$I->waitUntilTitleContains( 'Search Results' );
+
+		$this->assertTrue( true );
+	}
+
+}
