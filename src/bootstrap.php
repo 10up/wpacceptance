d0bc5f85c3f6fa921af32c9dee97b4752fef6492
Taylor Lovett <tlovett88@gmail.com>
2020-04-13 11:31:51 -0400

    Bump version again

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -9,7 +9,7 @@ namespace WPAcceptance;
 
 use \Symfony\Component\Console\Application;
 
-$app = new Application( 'WPAcceptance', '0.16.5' );
+$app = new Application( 'WPAcceptance', '0.16.6' );
 
 define( 'WPACCEPTANCE_DIR', dirname( __DIR__ ) );
 
