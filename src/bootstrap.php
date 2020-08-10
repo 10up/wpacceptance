e9cc1244c6b6718ae448bbfdb77bea04dccc2d2d
Tung Du <dinhtungdu@gmail.com>
2020-08-10 22:31:20 +0700

    chore: bump 0.16.8

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -9,7 +9,7 @@ namespace WPAcceptance;
 
 use \Symfony\Component\Console\Application;
 
-$app = new Application( 'WPAcceptance', '0.16.7' );
+$app = new Application( 'WPAcceptance', '0.16.8' );
 
 define( 'WPACCEPTANCE_DIR', dirname( __DIR__ ) );
 
