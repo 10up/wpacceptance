9a7b254cc66c8766dc1b752daf33a9033d910562
Taylor Lovett <tlovett88@gmail.com>
2021-01-15 12:36:49 -0500

    Version 0.16.9

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -9,7 +9,7 @@ namespace WPAcceptance;
 
 use \Symfony\Component\Console\Application;
 
-$app = new Application( 'WPAcceptance', '0.16.8' );
+$app = new Application( 'WPAcceptance', '0.16.9' );
 
 define( 'WPACCEPTANCE_DIR', dirname( __DIR__ ) );
 
